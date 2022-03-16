b13a5791e31356dca004a6f1a2ebcbd6a8cc2037
Gabriel Garcia <gokugamer190@gmail.com>
2022-03-15 22:23:03 -0300

    Adicionando aquivos da aula

---
+++ SitiosPadroesIII/3 - Exercicios/main.js
@@ -0,0 +1,9 @@
+
+
+function Calcular(){
+
+    let modelo_veiculo = document.getElementById("modelo").value;
+    let ano_fabricao = parseInt(document.getElementById("ano").value);
+    let data = new Date().getFullYear();
+    console.log(data); 
+}
---
+++ SitiosPadroesIII/RetornandoObjetos/main.js
@@ -0,0 +1,12 @@
+function calculaRetangulo(b, h) {
+    // var valor_area = (b * h);
+    // var valor_perimetro = (b + h) * 2;
+    // return {
+    //     area: valor_area,
+    //     perimetro: valor_perimetro
+    // };
+
+    let area = (b * h);
+    let perimetro = (b + h) * 2;
+    return [area, perimetro];
+};
