a8eee58b931c745e165b2c322b83a398092c83fd
Gabriel Garcia <gokugamer190@gmail.com>
2022-05-09 22:23:31 -0300

    Add portifolio

---
+++ 3º Semestre/Acessilidade/Meu Portifolio/main.js
@@ -0,0 +1,25 @@
+function trocarCores() {
+    modicarClassHero();
+
+}
+
+
+function modicarClassHero() {
+    document.getElementById('hero').style.cssText = "  background-image: url(Img/background_invertido.jpg);";
+    document.getElementById('lista').style.cssText = " color: black;";
+    document.getElementById('lista1').style.cssText = " color: black;";
+    document.getElementById('lista2').style.cssText = " color: black;";
+    document.getElementById('lista3').style.cssText = " color: black;";
+    document.getElementById('lista4').style.cssText = " color: black;";
+    document.getElementById('btn').style.cssText = " color: black; background-color: rgb(0,0,205);";
+    document.getElementById("span1").style.cssText = " color: rgb(0,0,205);";
+    document.getElementById("span").style.cssText = " color: rgb(0,0,205);";
+    document.getElementById('h1').style.cssText = " color: black";
+    document.getElementById('h4').style.cssText = " color: black;";
+    document.getElementById('h4').style.cssText = " color: black;";
+    document.getElementById('logo').style.cssText = " color: black;";
+}
+
+function modicarClassSobre(){
+    document.getElementById('hero').style.cssText = "  background-image: url(Img/background_invertido.jpg);";
+}
