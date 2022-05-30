9e7301d517c5455c5505f69dd0718b4fbe83ad4c
Gabriel Garcia <gokugamer190@gmail.com>
2022-05-29 23:55:53 -0300

    Terima a logica do formulario

--- 3º Semestre/SitiosPadroesIII/forms/main.js
+++ 3º Semestre/SitiosPadroesIII/forms/main.js
@@ -1,43 +1,97 @@
-function testarDissertativa(){
-    let perunta1 = document.getElementsByName("dissertativas").value;
 
-    let tam = perunta1.split(" ");
-
-    if( tam.length < 3 ){
-        alert("erro");
+function testarDissertativa() {
+    let pergunta = [];
+    for (i = 1; i <= 2; i++) {
+        pergunta.push(document.getElementById(i).value);
     }
-
+    let tam = []
+    let count = 0;
+    for (i = 0; i < pergunta.length; i++) {
+        pergunta;
+        tam[i] = pergunta[i].split(" ");
+        if (tam[i].length >= 3) {
+            count++;
+        }
+    }
+    return count;
 }
 
 function checarBox() {
-    var radio = document.getElementsByName("radio");
+    var lista = [];
+    var check1 = document.getElementsByName("check1");
+    var check2 = document.getElementsByName("check2");
+    var check3 = document.getElementsByName("check3");
 
-    for (var i = 0 in radio) {
-        if (radio[i].checked) {
-            alert("Escolheu: " + radio[i].value);
+    //VERIFICANDO CHECKBOX 1
+    for (var i = 0 in check1) {
+        if (check1[i].checked) {
+            lista.push(check1[i].value);
         }
     }
-}
+    let gabarito = ['0', '1'];
+    let count = 0;
+    for (let i = 0; lista[i] <= 4; i++) {
+        if (gabarito[i] == lista[i]) {
+            count++;
+        }
+    }
+    if (count < 2) {
+        count = 0;
+    }
+    ////////////////////////////////////////////
+    //VERIFICANDO CHECKBOX 2
+    lista = [];
+    gabarito = ['1', '2'];
+    for (var i = 0 in check2) {
+        if (check2[i].checked)
+            lista.push(check2[i].value);
+    }
 
-function checarRadio(){
-    var botoes = document.getElementsByName("flexRadioDefault");
-
-    for (var i = 0 in botoes) {
-        if (botoes[i].checked);
-        alert("Escolheu: " + botoes[i].value);
+    for (let i = 0; lista[i] <= 4; i++) {
+        if (gabarito[i] == lista[i]) {
+            count++;
+        }
+    }
+    if (count == 2 || count == 4) {
+    } else {
+        count = 0;
+    }
+    ////////////////////////////////////////////
+    //VERIFICANDO CHECKBOX 3
+    lista = [];
+    gabarito = ['0', '3'];
+    for (var i = 0 in check3) {
+        if (check3[i].checked)
+            lista.push(check3[i].value);
+    }
+    for (let i = 0; lista[i] <= 4; i++) {
+        if (gabarito[i] == lista[i]) {
+            count++;
+        }
+    }
+    if (count == 2 || count == 4 || count == 6) {
+        return count / 2;
+    } else {
+        return 0;
     }
 }
 
+// function checarRadio() {
+//     var botoes = document.getElementsByName("flexRadioDefault");
+//     for (var i = 0 in botoes)
+//         if (botoes[i].checked)
+//             alert("Escolheu: " + botoes[i].value);
+// }
 
-function enviar(){
 
-    var perunta = document.getElementById("dissertativas").value;
-    console.log(pergunta);
+function enviar() {
+    let contador_pontos = testarDissertativa() + checarBox();
+    console.log(contador_pontos);
 
-    let tam = perunta.split("");
+    // let count = checarBox();
+    // console.log(count);
 
-    if( tam.length < 3 ){
-        alert("erro");
-    }
+
+
 
 }
